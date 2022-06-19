10a50c997bc782552d8a6e246353d720272aa777
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-06-19 08:20:26 +0545

    Exception Handling Corrected

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,41 +1,50 @@
 import java.util.Scanner;
 
 class ArithmeticOperation {
-    private int a,b;
+    int a, b;
     Scanner scn = new Scanner(System.in);
-    void getNumbers() {
+
+    public void getNumbers() {
         System.out.println("Enter First Number:");
         a = scn.nextInt();
         System.out.println("Enter Second Number:");
         b = scn.nextInt();
     }
 
-     class ArithmeticInner{
-        void addition() {
-            System.out.println("Addition is:"+(a+b));
-        }
-        void subtraction() {
-            System.out.println("Subtraction is:"+(a-b));
-        }
-        void multiplication() {
-            System.out.println("Multiplication is:"+(a*b));
-        }
-        void division() {
-            System.out.println("Division is:"+(a/b));
-        }
-        void display() {
-            ArithmeticInner ai = new ArithmeticInner();
-            ai.addition();
-            ai.subtraction();
-            ai.multiplication();
-            ai.division();
+    void addition() {
+        System.out.println("Addition is:" + (a + b));
+    }
+
+    void subtraction() {
+        System.out.println("Subtraction is:" + (a - b));
+    }
+
+    void multiplication() {
+        System.out.println("Multiplication is:" + (a * b));
+    }
+
+    void division() {
+        try {
+            System.out.println("Division is:" + (a / b));
+        } catch (Exception e) {
+            System.out.println("error" + e.getMessage());
         }
     }
+
+    public void display() {
+        ArithmeticOperation ai = new ArithmeticOperation();
+        ai.addition();
+        ai.subtraction();
+        ai.multiplication();
+        ai.division();
+    }
+
 }
 
 class ExceptionHandling {
     public static void main(String[] args) {
         ArithmeticOperation ao = new ArithmeticOperation();
+        ao.getNumbers();
         ao.display();
     }
 }
